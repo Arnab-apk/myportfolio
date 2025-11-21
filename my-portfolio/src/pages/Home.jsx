4b932772482bf63fe4c0b2ec4bfae9e3f247bacd
Mr.Invictus <arnabmandal261@gmail.com>
2025-11-21 23:02:25 +0530

    feat: Add initial Home page with profile card, featured projects, and interactive UI elements.

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -108,8 +108,8 @@ const Home = () => {
 
                 {/* Scroll Indicator */}
                 <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-                    <div className="w-6 h-10 border-2 border-brand-yellow/50 rounded-full flex justify-center pt-2">
-                        <div className="w-1 h-3 bg-brand-yellow/50 rounded-full animate-pulse" />
+                    <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center pt-2">
+                        <div className="w-1 h-3 bg-white/50 rounded-full animate-pulse" />
                     </div>
                 </div>
             </div>
