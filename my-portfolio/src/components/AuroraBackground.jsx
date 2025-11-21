28412c2c62d27453408583675bfca7992c7e1bb8
Mr.Invictus <arnabmandal261@gmail.com>
2025-11-21 17:43:41 +0530

    feat: add AuroraBackground component

--- my-portfolio/src/components/AuroraBackground.jsx
+++ my-portfolio/src/components/AuroraBackground.jsx
@@ -1,24 +1,12 @@
+noiseIntensity = { 0.5}
+  />
+      </div >
 
-import Silk from "./Silk";
-
-function AuroraBackground({ children, className = "" }) {
-  return (
-    <div className={`relative ${className} bg-brand-dark min-h-[100dvh]`}>
-      {/* Silk WebGL Background */}
-      <div className="absolute inset-0 overflow-hidden">
-        <Silk
-          color="#FFD700"
-          speed={2}
-          scale={1.5}
-          noiseIntensity={0.5}
-        />
-      </div>
-
-      {/* Content */}
-      <div className="relative z-10">
-        {children}
-      </div>
-    </div>
+  {/* Content */ }
+  < div className = "relative z-10" >
+    { children }
+      </div >
+    </div >
   );
 }
 
