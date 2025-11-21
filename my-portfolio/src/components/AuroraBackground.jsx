0deafb762c126140ba1c87e3082e571a2f3d9769
Mr.Invictus <arnabmandal261@gmail.com>
2025-11-21 17:44:04 +0530

    feat: add AuroraBackground component for animated WebGL background

--- my-portfolio/src/components/AuroraBackground.jsx
+++ my-portfolio/src/components/AuroraBackground.jsx
@@ -1,12 +1,24 @@
-noiseIntensity = { 0.5}
-  />
-      </div >
 
-  {/* Content */ }
-  < div className = "relative z-10" >
-    { children }
-      </div >
-    </div >
+import Silk from "./Silk";
+
+function AuroraBackground({ children, className = "" }) {
+  return (
+    <div className={`relative ${className} min-h-[100dvh]`}>
+      {/* Silk WebGL Background */}
+      <div className="absolute inset-0 overflow-hidden">
+        <Silk
+          color="#FFD700"
+          speed={2}
+          scale={1.5}
+          noiseIntensity={0.5}
+        />
+      </div>
+
+      {/* Content */}
+      <div className="relative z-10">
+        {children}
+      </div>
+    </div>
   );
 }
 
